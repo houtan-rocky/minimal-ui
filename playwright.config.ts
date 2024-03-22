4c711c39097bf55880b6757f44472a46126ddb21
Houtan Rocky <mast9rmind@gmail.com>
2024-03-22 03:30:48 +0300

    fix: playwright webserver address

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -78,6 +78,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     reuseExistingServer: true,
-    port: 3000,
+    port: 8080,
   },
 });
