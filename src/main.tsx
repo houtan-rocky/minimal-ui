c5e459e2764c823157175c5c556eb0e941f95286
Houtan Rocky <mast9rmind@gmail.com>
2024-03-24 15:18:36 +0330

    feat: setup start function of service worker api

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,44 @@ import { HelmetProvider } from 'react-helmet-async';
 
 import App from './app';
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('src/_mock/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+    serviceWorker: {
+      url: '/mockServiceWorker.js',
+      options: {
+        scope: '/',
+        type: 'module',
+        updateViaCache: 'none',
+      },
+    },
+    findWorker(scriptUrl, mockServiceWorkerUrl) {
+      if (scriptUrl.includes(mockServiceWorkerUrl)) {
+        return true;
+      }
+      return false;
+    },
+  });
+}
 // ----------------------------------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <Suspense>
-        <App />
-      </Suspense>
-    </BrowserRouter>
-  </HelmetProvider>
-);
+enableMocking().then(() => {
+  root.render(
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
+});
