91886d0f55bf7005fc88cb74f158ae722a5f7480
Houtan Rocky <mast9rmind@gmail.com>
2024-03-25 01:59:28 +0330

    feat: refactor main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { Suspense } from 'react';
 import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
-import { Suspense, useEffect } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-import {
-  useLocation,
-  matchRoutes,
-  BrowserRouter,
-  useNavigationType,
-  createRoutesFromChildren,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
 import { enableMockingServiceWorker } from './_mock/utils/msw.util';
@@ -30,29 +24,19 @@ enableMockingServiceWorker().then(() => {
 });
 
 Sentry.init({
-  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  dsn: 'https://b8ff80def4ade7112df433ac72e1900f@o4506428773367808.ingest.us.sentry.io/4506968341872640',
   integrations: [
-    // See docs for support of different versions of variation of react router
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
     }),
-    Sentry.replayIntegration(),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  tracesSampleRate: 1.0,
-
-  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
