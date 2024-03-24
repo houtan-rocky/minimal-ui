355151811781c5a538cebb52b7da7d8bc1456f5f
Houtan Rocky <mast9rmind@gmail.com>
2024-03-25 01:55:54 +0330

    feat: install sentry and set it up to react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
-import { Suspense } from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
+import {
+  useLocation,
+  matchRoutes,
+  BrowserRouter,
+  useNavigationType,
+  createRoutesFromChildren,
+} from 'react-router-dom';
 
 import App from './app';
 import { enableMockingServiceWorker } from './_mock/utils/msw.util';
@@ -19,4 +27,32 @@ enableMockingServiceWorker().then(() => {
       </BrowserRouter>
     </HelmetProvider>
   );
+});
+
+Sentry.init({
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  integrations: [
+    // See docs for support of different versions of variation of react router
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
