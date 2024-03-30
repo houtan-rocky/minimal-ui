252008678e29f25adf7e875763f883de1f8ce778
Houtan Rocky <mast9rmind@gmail.com>
2024-03-30 14:59:13 +0330

    fix: remove extra provided argument for react profiler and fix vite config

--- src/pages/auth/new-password.page.tsx
+++ src/pages/auth/new-password.page.tsx
@@ -12,9 +12,7 @@ const onRenderCallback = (
   actualDuration: number,
   baseDuration: number,
   startTime: number,
-  commitTime: number,
-  interactions: any
-  // interactions: Set<React.ProfilerInteraction> // the Set of interactions belonging to this update
+  commitTime: number
 ) => {
   console.log('Profiling data for new_password_page:', {
     id,
@@ -23,7 +21,6 @@ const onRenderCallback = (
     baseDuration,
     startTime,
     commitTime,
-    interactions,
   });
 };
 export default function NewPasswordPage() {
--- src/pages/auth/verify-register.page.tsx
+++ src/pages/auth/verify-register.page.tsx
@@ -12,9 +12,7 @@ const onRenderCallback = (
   actualDuration: number,
   baseDuration: number,
   startTime: number,
-  commitTime: number,
-  interactions: any
-  // interactions: Set<React.ProfilerInteraction> // the Set of interactions belonging to this update
+  commitTime: number
 ) => {
   console.log('Profiling data for verify_register_page:', {
     id,
@@ -23,7 +21,6 @@ const onRenderCallback = (
     baseDuration,
     startTime,
     commitTime,
-    interactions,
   });
 };
 
