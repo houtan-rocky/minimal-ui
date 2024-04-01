43905ac25ea7b3d3dba6a2b2a6fa1c1636e79ce2
Houtan Rocky <mast9rmind@gmail.com>
2024-04-02 00:56:39 +0330

    feat: remove excessive features of navbar

--- src/layouts/dashboard/header.tsx
+++ src/layouts/dashboard/header.tsx
@@ -13,12 +13,10 @@ import Logo from 'src/components/logo';
 import SvgColor from 'src/components/svg-color';
 import { useSettingsContext } from 'src/components/settings';
 
-import Searchbar from '../common/searchbar';
 import { NAV, HEADER } from '../config-layout';
 import SettingsButton from '../common/settings-button';
 import AccountPopover from '../common/account-popover';
 import ContactsPopover from '../common/contacts-popover';
-import LanguagePopover from '../common/language-popover';
 import NotificationsPopover from '../common/notifications-popover';
 
 // ----------------------------------------------------------------------
@@ -52,8 +50,6 @@ export default function Header({ onOpenNav }: Props) {
         </IconButton>
       )}
 
-      <Searchbar />
-
       <Stack
         flexGrow={1}
         direction="row"
@@ -61,8 +57,6 @@ export default function Header({ onOpenNav }: Props) {
         justifyContent="flex-end"
         spacing={{ xs: 0.5, sm: 1 }}
       >
-        <LanguagePopover />
-
         <NotificationsPopover />
 
         <ContactsPopover />
